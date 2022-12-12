b1888879d9b48fcd74bd8a825de6e13497c7086f
xesvos <connect.with.siarhei@gmail.com>
2022-12-12 21:56:40 +0300

    fix: fix bugs with hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,14 +2,29 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-list");
 const navLinks = document.querySelectorAll(".nav-list__link");
 
+const darken = document.querySelector(".darken");
+
+// const main = document.querySelector(".main");
+// const footer = document.querySelector(".footer");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+
+  darken.classList.toggle("active");
+
+  // main.classList.toggle("darken");
+  // footer.classList.toggle("darken");
 });
 
 navLinks.forEach((link) =>
   link.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+
+    darken.classList.remove("active");
+
+    // main.classList.remove("darken");
+    // footer.classList.remove("darken");
   })
 );
