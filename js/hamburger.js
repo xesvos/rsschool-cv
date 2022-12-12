7fdfa8213ffd7760974b2bf4fcb9c177eeaa816c
xesvos <connect.with.siarhei@gmail.com>
2022-12-12 23:33:30 +0300

    refactor: remove commented code

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,17 +4,11 @@ const navLinks = document.querySelectorAll(".nav-list__link");
 
 const darken = document.querySelector(".darken");
 
-// const main = document.querySelector(".main");
-// const footer = document.querySelector(".footer");
-
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 
   darken.classList.toggle("active");
-
-  // main.classList.toggle("darken");
-  // footer.classList.toggle("darken");
 });
 
 navLinks.forEach((link) =>
@@ -23,8 +17,5 @@ navLinks.forEach((link) =>
     navMenu.classList.remove("active");
 
     darken.classList.remove("active");
-
-    // main.classList.remove("darken");
-    // footer.classList.remove("darken");
   })
 );
